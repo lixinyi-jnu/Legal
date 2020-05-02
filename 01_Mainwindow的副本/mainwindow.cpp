3a4d4bc405d7dc967cade97d4ed972c275c40b77
lixinyi <2237482161@qq.com>
2020-05-02 15:43:36 +0800

    实现菜单栏工具栏

---
+++ 01_Mainwindow的副本/mainwindow.cpp
@@ -0,0 +1,77 @@
+#include "mainwindow.h"
+#include <QMenuBar>
+#include <QToolBar>
+#include <QDebug>
+#include <QPushButton>
+#include <QStatusBar>
+#include <QLabel>
+#include <QDockWidget>
+#include <QTextEdit>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    //重置窗口大小
+    resize(600,400);
+
+    //菜单栏创建   菜单栏最多有一个
+    QMenuBar *bar = menuBar();
+    //将菜单栏放入窗口中
+    setMenuBar(bar);
+    //创建菜单
+    QMenu *fileMenu = bar->addMenu("文件");
+    QMenu *editMenu = bar->addMenu("编辑");
+
+    //创建菜单项
+    QAction *newAction =  fileMenu ->addAction("新建");
+    //添加分割线
+    fileMenu->addSeparator();
+    QAction *openAction = fileMenu->addAction("打开");
+
+    //工具栏 可以有多个
+    QToolBar *toolbar = new QToolBar(this);
+    addToolBar(Qt::LeftToolBarArea,toolbar);//固定在左边
+
+    //后期设置 只允许左右停靠
+    toolbar ->setAllowedAreas(Qt::LeftToolBarArea|Qt::RightToolBarArea);
+
+    //设置浮动
+    toolbar->setFloatable(false);
+
+    //设置移动（总开关）
+    toolbar->setMovable(false);
+
+    //工具栏中可以设置内容
+    toolbar->addAction(newAction);
+    toolbar->addSeparator();//添加分割线
+    toolbar->addAction(openAction);
+
+    //工具栏中添加控件
+    QPushButton *btn = new QPushButton("aa",this);
+    toolbar->addWidget(btn);
+
+    //状态栏 最多有一个
+    QStatusBar *stBar = statusBar();
+    //设置到窗口中
+    setStatusBar(stBar);
+    //放标签控件
+    QLabel *label = new QLabel("提示信息",this);
+    stBar->addWidget(label);
+    QLabel *label2 = new QLabel("右侧提示信息",this);
+    stBar->addPermanentWidget(label2);
+
+    //铆接部件（浮动窗口） 可以有多个
+    QDockWidget *dockWidget = new QDockWidget("浮动",this);
+    addDockWidget(Qt::BottomDockWidgetArea,dockWidget);
+    //设置后期停靠区域 只允许上下
+    dockWidget->setAllowedAreas(Qt::TopDockWidgetArea|Qt::BottomDockWidgetArea);
+
+    //设置中心部件 只能有一个
+    QTextEdit *edit = new QTextEdit(this);
+    setCentralWidget(edit);
+}
+
+MainWindow::~MainWindow()
+{
+}
+
